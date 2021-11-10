08aec042a926864580236e8d5c0c97df037720e5
Sheldon <sheldon.spencer@gmail.com>
2021-11-10 02:40:34 -0400

    basket services developed

---
+++ src/Services/Basket/Basket.API/Data/Entities/CacheSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data.Entities
+{
+    public class CacheSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
